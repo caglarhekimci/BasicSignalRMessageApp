3124f3f6538c2a7a3c058f44304cdef96efc5af9
Çağlar Hekimci <48187869+caglarhekimci@users.noreply.github.com>
2023-02-27 13:19:11 +0300

    Project Added
    Up to Date

---
+++ MessageAppSignalR/Hubs/MessageHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MessageAppSignalR.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public static List<string> _products = new();
+        public async Task SendProduct(string messageInfo)
+        {
+            _products.Add(messageInfo);
+            await Clients.All.SendAsync("ReceiveProduct", messageInfo, _products.Count());
+        }
+        public async Task ResetProduct()
+        {
+            _products.Clear();
+            await Clients.All.SendAsync("ReceiveResetProduct");
+        }
+    }
+}
---
+++ MessageAppSignalR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MessageAppSignalR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
